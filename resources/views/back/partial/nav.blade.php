6cd48ea6331a4bdf25c48ad886a297ec51a14d88
shuupsy <sylvaine.ruan@gmail.com>
2022-09-05 09:41:40 +0200

    NAV : changement icônes (parents)

--- resources/views/back/partial/nav.blade.php
+++ resources/views/back/partial/nav.blade.php
@@ -32,7 +32,6 @@
             <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
                 href="#">
                 <i class='bx bx-chat'></i>
-
                 <span class="mx-4 font-medium">Messagerie</span>
             </a>
 
