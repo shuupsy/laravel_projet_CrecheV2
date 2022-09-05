056a25f858466f4ae4d74d2b442df8f7f4fbd02f
Mounzz <senmounia@hotmail.com>
2022-09-05 11:37:48 +0200

    inventaire update

--- resources/views/back/pages/inventaire.blade.php
+++ resources/views/back/pages/inventaire.blade.php
@@ -1,15 +1,14 @@
 @extends('back.layout.index')
 @section('content')
     <div class="sizeIcon flex justify-center items-center h-screen">
-        <div class="grid lg:grid-cols-3 gap-16">
-
-
+        <div class="grid lg:grid-cols-3 gap-16 sm:grid sm:grid-cols-1">
             @foreach ($inventaire as $item)
-                <div class="w-96 mt-10 flex flex-wrap flex-col items-center gap-2 bg-white drop-shadow-md rounded-lg p-4">
-                    <div class="w-32 h-32">
-                        <img class="w-full" src="/assets/img/{{ $item->img }}">
+                <div class="w-80 mt-10 flex flex-wrap flex-col items-center gap-2 bg-white drop-shadow-md rounded-lg p-4">
+                    <div class="w-16 h-12">
+                        <img class="w-full -mt-8" src="/assets/img/{{ $item->img }}">
                     </div>
-                    <div class="mt-10">quantité restante: {{ $item->quantity }}</div>
+                    <div class="font-bold">{{ $item->name }}</div>
+                    <div>Quantité restante: {{ $item->quantity }}</div>
                     <div class="flex gap-4">
                         <form action="/backend/inventaire/diminuer" method="POST">
                             <button type="submit">-</button>
@@ -21,7 +20,6 @@
                             <button type="submit">+</button>
                         </form>
                     </div>
-                    <div>{{ $item->name }}</div>
                 </div>
             @endforeach
         </div>
