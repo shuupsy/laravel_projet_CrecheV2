07b4c720afcc3e37646a1fd0165a5081056f7ab9
shuupsy <sylvaine.ruan@gmail.com>
2022-09-05 10:47:48 +0200

    Changement emplacements images

--- database/seeders/BackaccueilSeeder.php
+++ database/seeders/BackaccueilSeeder.php
@@ -19,28 +19,28 @@ class BackaccueilSeeder extends Seeder
             [
                 "nom" => "Meunier",
                 "prenom" => "Henry",
-                "img" => "photo-1546015720-b8b30df5aa27.avif",
+                "img" => "team/photo-1546015720-b8b30df5aa27.avif",
                 "sexe" => "H",
                 "age" => 3
             ],
             [
                 "nom" => "Fischer",
                 "prenom" => "Claire",
-                "img" => "photo-1548897969-80be2924dc9f.avif",
+                "img" => "team/photo-1548897969-80be2924dc9f.avif",
                 "sexe" => "F",
                 "age" => 1
             ],
             [
                 "nom" => "Gimenes",
                 "prenom" => "Inès",
-                "img" => "photo-1608365151231-7dbed3034787.avif",
+                "img" => "team/photo-1608365151231-7dbed3034787.avif",
                 "sexe" => "F",
                 "age" => 2
             ],
             [
                 "nom" => "Leger",
                 "prenom" => "Jules",
-                "img" => "photo-1503454537195-1dcabb73ffb9.avif",
+                "img" => "team/photo-1503454537195-1dcabb73ffb9.avif",
                 "sexe" => "H",
                 "age" => 2
             ]
