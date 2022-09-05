d7a4b2727c72835175483ac78eecdb019ec837e3
shuupsy <sylvaine.ruan@gmail.com>
2022-09-05 10:39:08 +0200

    NAV : changement boutons

--- resources/views/front/partial/nav.blade.php
+++ resources/views/front/partial/nav.blade.php
@@ -48,12 +48,6 @@
 
                 <span class="mx-4 font-medium">Team</span>
             </a>
-            <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
-            href="{{ Route('backAccueil') }}">
-
-
-            <span class="mx-4 font-medium">Backend</span>
-        </a>
 
         </nav>
 
