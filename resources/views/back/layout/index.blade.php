6f3ccf34f91a4bbeaeefcef34cfc8e44dc849bee
shuupsy <sylvaine.ruan@gmail.com>
2022-09-05 09:26:48 +0200

    INDEX : link dans head

--- resources/views/back/layout/index.blade.php
+++ resources/views/back/layout/index.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    {{-- CSS --}}
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/media.css">
+    {{-- BoxIcons --}}
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <title>Document</title>
 </head>
 <body>
@@ -12,10 +17,13 @@
         <header>
             @include("back.partial.nav")
         </header>
+
         <main>
             @yield("content")
         </main>
     </div>
+
+    {{-- Link JS --}}
     <script src="/assets/js/modal.js"></script>
 </body>
 </html>
