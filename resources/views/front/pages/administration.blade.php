95e6a92eec59689b7f33fdac5576e1d47ac950ac
shuupsy <sylvaine.ruan@gmail.com>
2022-09-06 12:20:51 +0200

    Admin : balise form

--- resources/views/front/pages/administration.blade.php
+++ resources/views/front/pages/administration.blade.php
@@ -7,21 +7,23 @@
             <h1 class='text-3xl font-semibold text-gray-800 lg:text-4xl dark:text-white'>ADMINISTRATION</h1>
         </div>
 
-        {{-- Upload un document --}}
-        <div class='contenu flex flex-col gap-5'>
-            <div id="upload-cm" class='flex items-center gap-5'>
-                <label for="file-upload"
-                    class="flex items-center gap-3 px-6 py-2 font-medium tracking-wide text-white transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                    <i class='bx bx-download text-xl'></i>
-                    <p>Nouveau document</p>
-                </label>
-                <input type="file" name="image" id="file-upload" required>
-                <p class='text-md'>ex: certificats médicaux, ... </p>
-            </div>
+        <div class='contenu flex flex-col gap-10'>
+            {{-- Upload un document --}}
+            <form action="" method="post"enctype="multipart/form-data">
+                @csrf
+                <div id="upload-cm" class='flex items-center gap-5'>
+                    <label for="file-upload"
+                        class="flex items-center gap-3 px-6 py-2 font-medium tracking-wide text-white transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                        <i class='bx bx-download text-xl'></i>
+                        <p>Nouveau document</p>
+                    </label>
+                    <input type="file" name="image" id="file-upload" required>
+                    <p class='text-md'>ex: certificats médicaux, ... </p>
+                </div>
+            </form>
 
-
+            {{-- Documents à consulter --}}
             <div class='flex flex-wrap gap-5'>
-
                 {{-- Contrat --}}
                 <div class='flex items-center gap-5'>
                     <div
@@ -40,10 +42,10 @@
                     </div>
                     <p>Autorisation de sortie :
                         {{-- @if ($data->exitPermission == true)
-                                <i class='bx bxs-check-square text-2xl text-green-500'></i>
-                            @else
-                                <i class='bx bxs-x-square text-2xl text-red-500'></i>
-                            @endif --}}
+                                    <i class='bx bxs-check-square text-2xl text-green-500'></i>
+                                @else
+                                    <i class='bx bxs-x-square text-2xl text-red-500'></i>
+                                @endif --}}
                     </p>
                 </div>
 
@@ -55,10 +57,10 @@
                     </div>
                     <p>Autorisation de photos :
                         {{-- @if ($data->picturePermission == true)
-                                <i class='bx bxs-check-square text-2xl text-green-500'></i>
-                            @else
-                                <i class='bx bxs-x-square text-2xl text-red-500'></i>
-                            @endif --}}
+                                    <i class='bx bxs-check-square text-2xl text-green-500'></i>
+                                @else
+                                    <i class='bx bxs-x-square text-2xl text-red-500'></i>
+                                @endif --}}
                     </p>
                 </div>
 
@@ -70,7 +72,7 @@
                     </div>
                     <div>
                         {{-- <p>Jours de présence <span class='font-bold border border-dotted border-black rounded-full py-2 px-3'>{{ $data->presence }}</span></p>
-                            <p>{{ $data->dayOfPresence }}</p> --}}
+                                <p>{{ $data->dayOfPresence }}</p> --}}
                     </div>
                 </div>
 
