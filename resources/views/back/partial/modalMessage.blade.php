a069f832174863948c1f421f9209c5ba803ac3b9
Mounzz <senmounia@hotmail.com>
2022-09-06 12:13:49 +0200

    calendrier 30%

--- resources/views/back/partial/modalMessage.blade.php
+++ resources/views/back/partial/modalMessage.blade.php
@@ -1,16 +1,28 @@
-<button onclick="openBackMessage()" class="btnMessagerieBackOpen mx-32 my-10"></button>
-<div id="boxMessagerie" class="hidden boxMessagerieBack mx-36 my-14 ">
-    @foreach ($nurses as $item)
-        <div>
-            <button onclick="affichageIndividuel({{ $item->id }})" class="choixUsersMessagerie">{{ $item->nom }}
-                {{ $item->prenom }}</button>
-        </div>
-    @endforeach
-    @foreach ($messages as $item)
-        <div id="messageInd" class="hidden affichageBox" value="{{$item->frontaccueil_id}}">
+<button onclick="openBackMessage()" class="btnMessagerieBackOpen mx-32 my-10 shadow-md border border-slate-200"></button>
+
+<div id="boxMessagerie" class="hidden boxMessagerieBack mx-36 my-14 shadow-md border border-slate-200">
+    <div class="teamMessagerie">
+        @foreach ($nurses as $item)
+            <div class="bubbleTeam">
+                <div>
+                    <img class="imgTeam" src="/assets/img/team{{ $item->img }}" alt="">
+                </div>
+                <div>
+                    <button onclick="affichageIndividuel({{ $item->id }})"
+                        class="choixUsersMessagerie">{{ $item->nom }}
+                        {{ $item->prenom }}</button>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="flex flex-col gap-2 overflow-auto">
+        @foreach ($messages as $item)
+            <div id="messageInd" class="hidden affichageBox shadow-md" value="{{ $item->frontaccueil_id }}">
                 {{ $item->message }}
-        </div>
-    @endforeach
+            </div>
+        @endforeach
+    </div>
+
 </div>
 
 <script>
@@ -21,12 +33,12 @@
 
     function affichageIndividuel(x) {
         let boxMessage = document.querySelectorAll(".affichageBox")
-        boxMessage.forEach((index)=>{
+        boxMessage.forEach((index) => {
             index.classList.add("hidden")
         })
-        boxMessage.forEach((index)=>{
-            if(index.attributes.value.value == x)
-            index.classList.remove("hidden")
+        boxMessage.forEach((index) => {
+            if (index.attributes.value.value == x)
+                index.classList.remove("hidden")
         })
     }
 </script>
