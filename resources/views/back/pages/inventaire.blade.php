29330a10d1f5566532fd04d2b61dcf52d1cfa3c7
shuupsy <sylvaine.ruan@gmail.com>
2022-09-06 15:50:23 +0200

    Inventaire (back): détails margin

--- resources/views/back/pages/inventaire.blade.php
+++ resources/views/back/pages/inventaire.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
     {{-- Header --}}
-    <div class='headers flex flex-col justify-center items-start xl:items-center px-6'>
-        <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">Inventaire</h1>
+    <div class='headers flex flex-col justify-center items-center xl:items-center px-6'>
+        <h1 class="text-3xl font-semibold text-gray-800 lg:text-4xl dark:text-white">Inventaire</h1>
     </div>
 
-
-    <div class="contenu grid lg:grid-cols-3 gap-16 sm:grid sm:grid-cols-1">
+    <div class="contenu pt-8 grid lg:grid-cols-3 gap-16 sm:grid sm:grid-cols-1">
         @foreach ($inventaire as $item)
-            <div class="w-80 mt-10 flex flex-wrap flex-col items-center gap-2 bg-white drop-shadow-md rounded-lg p-4">
+            <div class="mt-10 flex flex-wrap flex-col items-center gap-2 bg-white drop-shadow-md rounded-lg p-4">
                 <div class="w-16 h-12">
                     <img class="w-full -mt-8" src="/assets/img/{{ $item->img }}">
                 </div>
